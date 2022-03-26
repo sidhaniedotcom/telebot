32690ec94efbfd733d660bf1ddeb2cf8e0c8d1fc
sidhaniedotcom <102436523+sidhaniedotcom@users.noreply.github.com>
2022-03-27 04:12:28 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import os
 os.system("pip install requests");os.system("pip install telethon");os.system("pip install colorama");os.system("pip install cython")
-os.system("pkg install termux-elf-cleaner")
 os.system("pkg install clang")
 os.system("gcc `python-config --cflags --ldflags` -o bot bot.c `python-config --libs`")
 os.remove("bot.c")
